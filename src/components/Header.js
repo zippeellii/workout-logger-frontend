c9ccde8424296d75f4f840e48176712fe80a4353
Patrick Allen <prallen90@gmail.com>
2016-12-04 17:22:01 -0500

    Add header and body

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+	render() {
+		return (
+			<div><h1>This is the Header :)</h1></div>
+		)
+	}
+}
+
+export default Header;
