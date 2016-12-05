37b02be460d9a1062681fcc7119c8e5a2c3f1963
Patrick Allen <prallen90@gmail.com>
2016-12-04 22:10:27 -0500

    Add call to login endpoint

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
 import LoginForm from './Forms/LoginForm';
 import {Col} from 'react-bootstrap';
+import request from 'request';
 
 
 class Login extends Component {
-	constructor() {
-		super();
-	}
-
 	login(userInfo) {
 		let email = userInfo.email;
 		let password = userInfo.password;
-		console.log(`Submitting for email: ${email}`)
+
+		request.post({
+			url: "http://localhost:8080/users/login", 
+			json: {
+				email: email, password: password
+			},
+		}, (err, res, body) => {
+			if(!err && res.statusCode === 200) {
+				console.log("Success!");
+			} else {
+				console.log(err, res.statusCode, body);
+			}
+		});
 	}
 
 	render() {
